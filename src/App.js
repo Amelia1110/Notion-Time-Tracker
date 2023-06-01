45593157d1397e7586812907384f508a9f49681b
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-06-01 14:04:34 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <header>
       <div className="bg-blue-500 w-[250px] h-[250px] rounded-md"/>
     </header>
   );
 }
-
-export default App;
