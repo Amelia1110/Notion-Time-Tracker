2225082c6c10551ff84bdb7f36cc599035ab570a
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-06-01 23:25:59 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { ClockCircleOutlined } from '@ant-design/icons';
 
 export default function App() {
   return (
-    <header>
-      <div className="bg-blue-500 w-[250px] h-[250px] rounded-md"/>
+    <header className="flex flex-row gap-1 mt-1 ml-1">
+      <Tab/>
+      <Tab/>
+      <hr className="border-black"/>
     </header>
+  );
+}
+
+function Tab() {
+  return (
+    <div className="bg-gray-200 h-full py-1 px-2 font-serif rounded-md flex flex-row items-center gap-1.5">
+      <ClockCircleOutlined/>
+      <p>Track</p>
+    </div>
   );
 }
