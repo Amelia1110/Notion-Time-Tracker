a7e6728167f33e91592133baadc07ec84c1cf66d
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-06-02 19:05:26 -0400

    Set states with header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import './App.css';
 import { ClockCircleOutlined } from '@ant-design/icons';
+import { CalendarOutlined } from '@ant-design/icons';
 import React from 'react';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [state, setState] = useState("Track")
-  useEffect(()=>{
-    console.log(`Hello John <3, the current state is ${state}`)
-  }, [state])
+  const [tabState, setTabState] = useState("Track")
+  
+  useEffect(() => {
+    console.log(`Hello John <3, the current state is ${tabState}`)
+  }, [tabState])
+
+  function switchTab(tab) {
+    setTabState(tab)
+  }
 
   return (
-    <header className="flex flex-row gap-1 mt-1 ml-1">
-      <Tab text="Track"/>
-      <Tab text="Overview"/>
+    <header>
+      <div className="flex flex-row gap-1 m-1">
+        <Tab tabName="Track" onClickTab={() => switchTab("Track")}/>
+        <Tab tabName="Overview" onClickTab={() => switchTab("Overview")}/>
+      </div>
       <hr className="border-black"/>
-      <button onClick={()=>{
-        setState("Overview")
-      }} >Hello</button>
     </header>
   );
 }
 
-function Tab({text}) {
+function Tab({tabName, onClickTab}) {
+  // Active button colour should be bg-grey-200
+
   return (
-    <div className="bg-gray-200 h-full py-1 px-2 font-serif rounded-md flex flex-row items-center gap-1.5">
-      <ClockCircleOutlined/>
-      <p>{text}</p>
-    </div>
+    <button className="hover:bg-blue-100 h-full py-1 px-2 font-serif rounded-md flex flex-row items-center gap-1.5" onClick={onClickTab}>
+      <Icon iconDisplayed={tabName}/>
+      <p>{tabName}</p>
+    </button>
   );
+  }
+
+function Icon(props) {
+  if (props.iconDisplayed === "Track") {
+    return <ClockCircleOutlined/>
+  }
+  if (props.iconDisplayed === "Overview") {
+    return <CalendarOutlined/>
+  }
 }
