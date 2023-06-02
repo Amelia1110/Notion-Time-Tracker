5eae6753e0ca32ab9093450bb2759af039043da8
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-06-01 23:39:16 -0400

    worked on header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
 import { ClockCircleOutlined } from '@ant-design/icons';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [state, setState] = useState("Track")
+  useEffect(()=>{
+    console.log(`Hello John <3, the current state is ${state}`)
+  }, [state])
+
   return (
     <header className="flex flex-row gap-1 mt-1 ml-1">
-      <Tab/>
-      <Tab/>
+      <Tab text="Track"/>
+      <Tab text="Overview"/>
       <hr className="border-black"/>
+      <button onClick={()=>{
+        setState("Overview")
+      }} >Hello</button>
     </header>
   );
 }
 
-function Tab() {
+function Tab({text}) {
   return (
     <div className="bg-gray-200 h-full py-1 px-2 font-serif rounded-md flex flex-row items-center gap-1.5">
       <ClockCircleOutlined/>
-      <p>Track</p>
+      <p>{text}</p>
     </div>
   );
 }
